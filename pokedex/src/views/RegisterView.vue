59b7d786c12712d129dac30fbffcf7e2a54f1561
David Kalmuk <correo@correo.com>
2025-02-06 19:29:56 +0100

    Ahora el register funciona, y envia el correo bien

--- pokedex/src/views/RegisterView.vue
+++ pokedex/src/views/RegisterView.vue
@@ -3,11 +3,11 @@
     <form @submit.prevent="registrar">
         <div class="">
             <label for="">Email</label>
-            <input type="email" placeholder="Tu correo">
+            <input type="email" placeholder="Tu correo" v-model="email">
         </div>
         <div class="">
             <label for="">Password</label>
-            <input type="password" placeholder="Tu contraseña">
+            <input type="password" placeholder="Tu contraseña" v-model="password">
         </div>
         <p>¿Ya tienes cuenta? <router-link to="/login">Iniciar Sesión</router-link></p>
         <button class="">Crear cuenta</button>
@@ -16,8 +16,28 @@
 </template>
 
 <script>
+import { createUserWithEmailAndPassword,sendEmailVerification } from 'firebase/auth'
+import {auth} from '../db/firebase.js'
 export default {
-    name:'RegisterView'
+    name:'RegisterView',
+    data(){
+        return {
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        async registrar(){
+           try {
+            const userCredentials=await createUserWithEmailAndPassword(auth,this.email,this.password) 
+            sendEmailVerification(userCredentials.user)
+            alert('Registrado correctamente')
+           } catch (error) {
+            alert(error.message)
+           }
+           
+        }
+    }
 }
 </script>
 
