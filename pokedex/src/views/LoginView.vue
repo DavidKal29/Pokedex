edb6e165ab5bd30649ea5486d9477e8e53fc0805
David Kalmuk <correo@correo.com>
2025-02-06 19:34:36 +0100

    El login tambien funciona ahora

--- pokedex/src/views/LoginView.vue
+++ pokedex/src/views/LoginView.vue
@@ -3,11 +3,11 @@
     <form @submit.prevent="logear">
         <div class="">
             <label for="">Email</label>
-            <input type="email" placeholder="Tu correo">
+            <input type="email" placeholder="Tu correo" v-model="email">
         </div>
         <div class="">
             <label for="">Password</label>
-            <input type="password" placeholder="Tu contraseña">
+            <input type="password" placeholder="Tu contraseña" v-model="password">
         </div>
         <p>¿No tienes cuenta? <router-link to="/register">Crear cuenta</router-link></p>
         <button class="">Iniciar Sesión</button>
@@ -16,8 +16,31 @@
 </template>
 
 <script>
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../db/firebase.js'
 export default {
-    name:'LoginView'
+    name:'LoginView',
+    data(){
+        return {
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        async logear(){
+            try {
+                const userCredentials=await signInWithEmailAndPassword(auth,this.email,this.password)
+                if (!userCredentials.user.emailVerified) {
+                    alert('Estas logueado, pero estas sin verificar')
+                }else{
+                    alert('Estas logueado y verificado')
+                }
+            } catch (error) {
+                alert(error.message)
+            }
+
+        }
+    }
 }
 </script>
 
