2c26ad7803657c297f1980d4bbb956616795e398
David Kalmuk <correo@correo.com>
2025-02-08 23:08:26 +0100

    Hacemos la interfaz de home

--- pokedex/src/views/HomeView.vue
+++ pokedex/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
-  <section class="">
-    <img src="https://pipedream.com/s.v0/app_mvNhzR/logo/orig" alt="">
-    <button><router-link to="/pokedex">Comenzar</router-link></button>
+  <section class="flex flex-col justify-content items-center h-screen bg-blue-700">
+    <div class="flex flex-col gap-[20px] mt-[100px]">
+      <img src="https://pipedream.com/s.v0/app_mvNhzR/logo/orig" alt="">
+      <button class="rounded bg-blue-500 text-yellow-300 font-bold py-[8px] px-[16px]"><router-link to="/pokedex">Comenzar a buscar</router-link></button>
+    </div>
   </section>
 </template>
 
